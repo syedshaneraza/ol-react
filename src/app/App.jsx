08e541a0e45b2a9769f68b217fd1333a202094d8
syedshaneraza <syed.shaneraza@phaedrasolutions.com>
2022-05-13 11:11:58 +0500

    map-controls-added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,34 +1,59 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import 'ol/ol.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import "ol/ol.css";
+import {
+  defaults,
+  ScaleLine,
+  Rotate,
+  FullScreen,
+  ZoomSlider,
+  ZoomToExtent,
+  MousePosition,
+  OverviewMap,
+  Attribution
+} from "ol/control";
 
-function App() {
+  function App() {
     const [map, setMap] = useState();
     const mapElement = useRef();
     const mapRef = useRef();
     mapRef.current = map;
 
     useEffect(() => {
-        const initialMap = new Map({
-          target: mapElement.current,
-            layers: [
-                new TileLayer({
-                    source: new OSM(),
-                }),
-            ],
-            view: new View({
-                center: [30, 70],
-                zoom: 10,
-            }),
-        });
-        setMap(initialMap);
+      const initialMap = new Map({
+        target: mapElement.current,
+        controls: defaults().extend([
+            new ScaleLine(),
+            new Rotate(),
+            new FullScreen(),
+            new ZoomSlider(),
+            new ZoomToExtent(),
+            new MousePosition(),
+            new OverviewMap(),
+            new Attribution()
+          ]),
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        view: new View({
+          center: [7000000, 3000000],
+          zoom: 3,
+        }),
+      });
+      setMap(initialMap);
     }, []);
 
     return (
-      <div style={{height:'100vh',width:'100%'}} ref={mapElement} className="map-container" />
+      <div
+        style={{ height: "100vh", width: "100%" }}
+        ref={mapElement}
+        className="map-container"
+      />
     );
-}
+  };
 
 export default App;
